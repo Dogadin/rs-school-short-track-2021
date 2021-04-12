866723953bcd341d6bd2e846ba74580ae88e100e
Александр Догадин <dogadin@tut.by>
2021-04-12 22:16:55 +0300

    refactor: queue

--- src/14-queue.js
+++ src/14-queue.js
@@ -12,40 +12,16 @@
  *
  */
 const ListNode = require('../extensions/list-node');
+
 class Queue {
-  constructor() {
-    this.arr = [];
-  }
 
   get size() {
-    return this.arr.length;
   }
 
   enqueue(element) {
-    this.arr.push(element);
   }
 
   dequeue() {
-    return this.arr.shift();
-    // return this.arr.pop();
-  }
-
-  convertArrayToList(arr) {
-    return arr.reverse().reduce((acc, cur) => {
-      if (acc) {
-        const node = new ListNode(cur);
-        node.next = acc;
-        return node;
-      }
-      return new ListNode(cur);
-    }, null);
-  }
-
-  convertListToArray(list, arr = []) {
-    const { value, next } = list;
-    arr.push(value);
-    if (next === null) return arr;
-    return this.convertListToArray(next, arr);
   }
 
 }
